6d9c58c675056f568237778f3f90988a9a55745d
Omaral22 <omaralobaid22@gmail.com>
2022-01-26 14:51:14 +0300

    add ActivitiesSummary faker

--- src/generators/Fitbit/ActivitiesSummary Faker.js
+++ src/generators/Fitbit/ActivitiesSummary Faker.js
@@ -1,9 +1,8 @@
 let faker = require("faker");
 const { parse } = require("json2csv");
-let activity = ["soccer","basketball","tennis","baseball","golf","running","volleyball","badminton","swimming","boxing","table tennis","skiing","ice skating","roller skating","cricket","rugby","pool","darts","football","bowling","ice hockey","surfing","karate","horse racing","snowboarding","skateboarding","cycling","archery","fishing","gymnastics","figure skating","rock climbing","sumo wrestling","taekwondo","fencing","water skiing","jet skiing","weight lifting","scuba diving","judo","wind surfing","kickboxing","sky diving","hang gliding","bungee jumping"];
+ let activity = ["soccer","basketball","tennis","baseball","golf","running","volleyball","badminton","swimming","boxing","table tennis","skiing","ice skating","roller skating","cricket","rugby","pool","darts","football","bowling","ice hockey","surfing","karate","horse racing","snowboarding","skateboarding","cycling","archery","fishing","gymnastics","figure skating","rock climbing","sumo wrestling","taekwondo","fencing","water skiing","jet skiing","weight lifting","scuba diving","judo","wind surfing","kickboxing","sky diving","hang gliding","bungee jumping"];
  const fields = [
-    "activeduration","activezoneminutes","activitylevel","activityname","activitytypeid","averageheartrate","calories","calorieslink","duration","elevationgain","hasactivezoneminutes","heartratelink","heartratezones","lastmodified","logid","logtype","manualvaluesspecified","originalduration","originalstarttime","starttime","tcxlink","distance","distanceunit","pace","source","speed","steps","user","day","period"
-];
+    "activityscore","activitycalories","caloriesbmr","caloriesout","distances","elevation","fairlyactiveminutes","floors","heartratezones","lightlyactiveminutes","marginalcalories","restingheartrate","sedentaryminutes","steps","veryactiveminutes","user","day"];
 const opts = { fields };
 var sync = true; 
 
@@ -11,11 +10,8 @@ var sync = true;
 for (let i = 0; i < 30; i++) {
   let date = faker.date.past();
   let date_end = new Date(date.getTime());
-   date_end.setHours(date.getHours() + 24);
-  
-
-
-   
+  date_end.setHours(date.getHours() + 24);
+ 
 
   let minuteMultiplierItem = {
     minuteMultiplier:getFakeNumber(0, 100),
@@ -37,13 +33,13 @@ for (let i = 0; i < 30; i++) {
      name :'sedentary',
     
   };
-   
+ 
 
     
-  let distance = {
+  let distanceItem = {
     
     activity:"text",
-            "distance":getFakeNumber(0, 100)
+    distance:getFakeNumber(0, 100)
     
   };
   let heartRateZone = {
@@ -56,15 +52,20 @@ for (let i = 0; i < 30; i++) {
     
   };
 
-  let manualValuesSpecifiedItem = {
-     calories :false,
-     distance :false,
-     steps :false,
-  };
 
 
   let summary = {
-    activeDuration :getFakeNumber(0, 10000000),
+    activeScore :getFakeNumber(-1, 5),
+    activityCalories:getFakeNumber(0, 1000),
+    caloriesBMR:getFakeNumber(0, 1000),
+    caloriesOut:getFakeNumber(0, 1000),
+    distances: [distanceItem,distanceItem,distanceItem],
+    elevation: getFakeNumber(0, 1000),
+    fairlyActiveMinutes:getFakeNumber(0, 1000),
+    floors:getFakeNumber(0, 50),
+    heartRateZones:[  heartRateZone ,heartRateZone,heartRateZone],
+
+
 
     activeZoneMinutes: [ activeZoneMinutesItem,activeZoneMinutesItem,activeZoneMinutesItem,],
     activityLevel:[activityLevelItem,activityLevelItem,activityLevelItem,activityLevelItem,activityLevelItem],
@@ -77,27 +78,21 @@ for (let i = 0; i < 30; i++) {
     elevationGain: getFakeNumber(0, 1000),
     hasActiveZoneMinutes:'true',
 
-     heartRateZones:[  heartRateZone ,heartRateZone,heartRateZone],
-    distances: [distance,distance,distance],
+    distances: [distanceItem,distanceItem,distanceItem],
     heartRateLink:'http',
 
-    heartRateZones:[  heartRateZone ,heartRateZone,heartRateZone],
+    lightlyActiveMinutes: getFakeNumber(0, 1000),
+    marginalCalories: getFakeNumber(0, 1000),
+    restingHeartRate: getFakeNumber(0, 1000),
+    sedentaryMinutes: getFakeNumber(0, 1000),
+    steps: getFakeNumber(0, 10000),
+    veryActiveMinutes: getFakeNumber(0, 100),
 
-    lastModified:date,
-    logId:getFakeNumber(0, 10000000000000),
-    logType:"auto_detected",
-    manualValuesSpecified:manualValuesSpecifiedItem,
-    originalDuration:getFakeNumber(0, 10000000),
-    originalStartTime: faker.date.past(),
-    startTime:faker.date.past(),
-    steps:getFakeNumber(0,100000),
-    tcxLink:'http',
-
+ 
  
   };
   
 
-  
   
   if (sync) getJSON(summary);
     else getCSV(summary);
