53eea324b43d3dceea30473745ca4909c1cda0d7
Omaral22 <omaralobaid22@gmail.com>
2023-10-11 12:56:33 +0300

    health

--- src/generators/health/vital.js
+++ src/generators/health/vital.js
@@ -1,20 +1,31 @@
 const faker = require('faker');
 
-const generateVitalSigns = () => {
+const generateVitalSigns = (date) => {
   const vitalSigns = {
     heart_rate: faker.datatype.number({ min: 60, max: 120 }), // Heart rate in beats per minute
-    breaths: faker.datatype.number({ min: 12, max: 30 }), // Breaths per minute
+    breaths: faker.datatype.number({ min: 20, max: 60 }), // Breaths per minute
     temperature: faker.datatype.float({ min: 36.0, max: 40 }), // Temperature in Celsius
     blood_pressure: `${faker.datatype.number({ min: 90, max: 140 })}/${faker.datatype.number({ min: 60, max: 80 })}`, // Blood pressure in mmHg
     oxygen_saturation: faker.datatype.number({ min: 95, max: 100 }), // Oxygen saturation in percentage
-    timestamp:  new Date().toISOString() // Current date and time in ISO format
-
-    };
+    timestamp: date.toISOString() // Use provided date
+  };
 
   return vitalSigns;
 };
 
-const vitalSignsData = generateVitalSigns();
+const generateDataForDays = (days) => {
+  const records = [];
+  let currentDate = new Date();
+
+  for (let i = 0; i < days; i++) {
+    records.push(generateVitalSigns(currentDate));
+    currentDate.setDate(currentDate.getDate() - 1); // Move back one day
+  }
+
+  return records;
+};
+
+const vitalSignsData = generateDataForDays(10);
 const json = JSON.stringify(vitalSignsData, null, 2);
 
 console.log(json);
